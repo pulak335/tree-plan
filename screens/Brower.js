3b73536fa3d13e210e4d534e1a87ba9d15cfa454
Arafat Islam <30324265+pulak335@users.noreply.github.com>
2022-07-17 13:28:04 +0600

    first commit

---
+++ screens/Brower.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Brower = () => {
+  return (
+    <View>
+      <Text>Brower</Text>
+    </View>
+  )
+}
+
+export default Brower
+
+const styles = StyleSheet.create({})
